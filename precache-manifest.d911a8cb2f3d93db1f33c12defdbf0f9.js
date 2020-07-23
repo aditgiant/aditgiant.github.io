dbcc071cdf18a5d7b6d4e967cd1388cddc530f04
aditgiant <adityagianto@gmail.com>
2020-07-23 15:00:50 +0200

    Updates

---
+++ precache-manifest.d911a8cb2f3d93db1f33c12defdbf0f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b365833f70bdef7838e34f76e95c7b02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "228407d4f8638a9a31ba",
+    "url": "/static/css/main.44120d61.chunk.css"
+  },
+  {
+    "revision": "4937c031bdfdcdf69011",
+    "url": "/static/js/2.cd570dfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cd570dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228407d4f8638a9a31ba",
+    "url": "/static/js/main.b31e05d1.chunk.js"
+  },
+  {
+    "revision": "e17f14aba305564c2ab9",
+    "url": "/static/js/runtime-main.1e8cb3c9.js"
+  },
+  {
+    "revision": "1743b28eb1023e2ad02d58b6aebccfd2",
+    "url": "/static/media/Profpict.1743b28e.jpg"
+  },
+  {
+    "revision": "630f431473fe8332c19afd9294d0a073",
+    "url": "/static/media/Welcome2.630f4314.png"
+  }
+]);
